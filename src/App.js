5557bbd829541edcc64db05e3a8462fcba474b9e
Chih-chi Li <t107590452@ntut.org.tw>
2020-11-17 18:46:22 +0800

    home page and appbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import CameraIcon from '@material-ui/icons/PhotoCamera';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
+import Avatar from '@material-ui/core/Avatar';
+import Popover from '@material-ui/core/Popover';
 import { Nav,Navbar,NavDropdown,Form,FormControl,Button } from 'react-bootstrap';
-import Toolbar from '@material-ui/core/Toolbar';
-import Breadcrumbs from '@material-ui/core/Breadcrumbs';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
@@ -35,13 +32,24 @@ function handleClick(event) {
   console.info('You clicked a breadcrumb.');
 }
 
+
 const useStyles = makeStyles((theme) => ({
   icon: {
     marginRight: theme.spacing(2),
   },
+  homecoverdiv: {
+    display:'flex',
+    alignItems:'center',
+    justifyContent:'center',
+    width:'100%',
+  },
+  homecover: {
+    width:'100%',
+  },
   heroContent: {
     backgroundColor: theme.palette.background.paper,
     padding: theme.spacing(8, 0, 6),
+    backgroundColor: '#f7ebea',
   },
   heroButtons: {
     marginTop: theme.spacing(4),
@@ -65,6 +73,9 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.background.paper,
     padding: theme.spacing(6),
   },
+  iconColor: {
+    backgroundColor:'#9e7d7a',
+  }
 }));
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -72,24 +83,72 @@ const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 export default function Album() {
   const classes = useStyles();
+  
+
+
+  const [anchorProfile, setAnchorProfile] = React.useState(null);
+
+  const handleProfileClick = (event) => {
+    setAnchorProfile(event.currentTarget);
+  };
+
+  const handleProfileClose = () => {
+    setAnchorProfile(null);
+  };
+
 
   return (
     <React.Fragment>
-      <Navbar className="brand-bar">
+      <Navbar className="brand-bar" style={{justifyContent:'space-between'}}>
         <Navbar.Brand href="#home">Stella Boutique</Navbar.Brand>
+          <Avatar className={classes.iconColor} src="/broken-image.jpg" onClick={handleProfileClick} id="profile-icon"/>
+          <Popover
+            open={Boolean(anchorProfile)}
+            margin='100px'
+            anchorEl={anchorProfile}
+            onClose={handleProfileClose}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'right',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            style={{
+              width: '150%',
+              height: '150%',
+            }}
+          >
+            
+          </Popover>
       </Navbar>
       <Navbar bg="dark" variant="dark" className="menu-bar" expand="lg">
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            <Nav.Link href="#home">NEW ARRIVALS</Nav.Link>
+            <Nav.Link href="#link">ON SALE</Nav.Link>
+            <NavDropdown title="TOPS" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Shirt</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">T-Shirt</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Hoodie</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Tank Tops</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="BOTTOMS" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Pants</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Jeans</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Skirt</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Leggings</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Shorts</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="OUTERWEAR" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Coat</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Jacket</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="DRESSES &JUMPSUITS" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Dresses</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Jumpsuits</NavDropdown.Item>
             </NavDropdown>
           </Nav>
           <Form inline>
@@ -102,25 +161,15 @@ export default function Album() {
         {/* Hero unit */}
         <div className={classes.heroContent}>
           <Container maxWidth="sm">
-            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-              Stella Boutique
-            </Typography>
-            <Typography variant="h5" align="center" color="textSecondary" paragraph>
-              Something short and leading about the collection below—its contents, the creator, etc.
-              Make it short and sweet, but not too short so folks don&apos;t simply skip over it
-              entirely.
-            </Typography>
+            <div className="homecoverdiv">
+              <img src={require('./img/cover.jpg')} alt="Background" className={classes.homecover} />
+            </div>
             <div className={classes.heroButtons}>
               <Grid container spacing={2} justify="center">
                 <Grid item>
                   <Button variant="contained" color="primary">
                     Shopping GOGO
                   </Button>
-                </Grid>
-                <Grid item>
-                  {/* <Button variant="outlined" color="primary">
-                    Secondary action
-                  </Button> */}
                 </Grid>
               </Grid>
             </div>
