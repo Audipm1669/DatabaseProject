fe1d157157e911feefd1a671a233d343b79dfbcb
Qi-Yu Wu <t107590061@ntut.edu.tw>
2020-12-07 19:53:23 +0800

    server added

---
+++ stella_boutique_server/src/main/java/com/project/stella_boutique/StellaBoutiqueServerApplication.java
@@ -0,0 +1,13 @@
+package com.project.stella_boutique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StellaBoutiqueServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StellaBoutiqueServerApplication.class, args);
+	}
+
+}
