efc161f8e409ffe9c2327488d55dede2d4e5e4ea
Fiveman0830 <t107590013@ntut.org.tw>
2020-12-28 03:02:58 +0800

    There is no props in the function's reference, middleware need it

--- stella_boutique_server/src/main/java/com/project/stella_boutique/service/management/Login.java
+++ stella_boutique_server/src/main/java/com/project/stella_boutique/service/management/Login.java
@@ -1,40 +1,46 @@
 package com.project.stella_boutique.service.management;
 
+import com.project.stella_boutique.adapter.database.MysqlDriver;
+import com.project.stella_boutique.model.user.User;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 @Service
 public class Login {
     @Autowired
     private MysqlDriver mysqlDriver = new MysqlDriver();
 
-    @override
-    public User findByEmail(String username, String password) {
-        Connection connection = null;
+    public User findByEmail(String usernameInput, String passwordInput) {
         User user = null;
 
         try(Connection connection = this.mysqlDriver.getConnection()) {
             try (PreparedStatement stmt = connection.prepareStatement(
                     "SELECT * FROM `user` WHERE `username` = ? and `password` = ?")) {
-                    stmt.setString(1, username);
-                    stmt.setString(2, password);
+                    stmt.setString(1, usernameInput);
+                    stmt.setString(2, passwordInput);
 
                     try (ResultSet rs = stmt.executeQuery()) {
                         while(rs.next()) {
-                            int id = rs.getInt("id");
+                            int id = Integer.parseInt(rs.getString("id"));
                             String password = rs.getString("password");
                             String fullName = rs.getString("fullname");
                             String username = rs.getString("username");
                             String birthday = rs.getString("birthday");
                             String address = rs.getString("address");
                             String phoneNumber = rs.getString("phoneNumber");
-                            String email = result.getString("email"); 
+                            String email = rs.getString("email"); 
                             
-                            user = new User(id, password, fullName, birthday, address, phoneNumber, email);
+                            user = new User(id, password, fullName, username, birthday, address, phoneNumber, email);
                         }
                     }
                 }
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            this.mysqlDriverAdapter.closeConnection(connection);
         }
         return user;
     }   
--- stella_boutique_server/src/main/java/com/project/stella_boutique/service/management/Register.java
+++ stella_boutique_server/src/main/java/com/project/stella_boutique/service/management/Register.java
@@ -1,14 +1,21 @@
 package com.project.stella_boutique.service.management;
 
+import com.project.stella_boutique.adapter.database.MysqlDriver;
+import com.project.stella_boutique.model.user.User;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 @Service
 public class Register {
     @Autowired
     private MysqlDriver mysqlDriver = new MysqlDriver();
 
-    @override
     private void registerUser(String username, String password, String fullname, String birthday, String address, String phoneNumber, String email) {
-        Connection connection = null;
-
         try(Connection connection = this.mysqlDriver.getConnection()) {
             try (PreparedStatement stmt = connection.prepareStatement(
                     "INSERT INTO `user`" +
@@ -17,7 +24,7 @@ public class Register {
                 )) {
                     stmt.setString(1, "NULL");
                     stmt.setString(2, password);
-                    stmt.setString(3, fullName);
+                    stmt.setString(3, fullname);
                     stmt.setString(4, username);
                     stmt.setString(5, birthday);
                     stmt.setString(6, address);
@@ -28,8 +35,6 @@ public class Register {
                 }
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            this.mysqlDriverAdapter.closeConnection(connection);
         }
     }   
 }
