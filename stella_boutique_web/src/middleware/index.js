412ae5d750dad314bf8697507a8d62903810a7b0
stelfalen <stellstellfalen@gmail.com>
2021-01-06 02:42:48 +0800

    add discount

--- stella_boutique_web/src/middleware/index.js
+++ stella_boutique_web/src/middleware/index.js
@@ -100,6 +100,18 @@ const myMiddleware = store => next => action => {
             console.log(err)
             alert("Entering Web Failed!")
         });
+
+        console.log("middleware get discount");
+        axios.get(API_HOST + '/guest/discount', body, {headers: headers})
+        .then(response => {
+            action.setUserDiscountList(response.data.discountList,store.dispatch);
+            console.log("discount list " +response.data.discountList )
+        })
+        .catch(err => {
+            console.log(err)
+            alert("Get Discount Failed!")
+        });
+
     } else if(action.type === "ADD_ORDER"){
         const headers = getHeaders(action.token);
         const body = {
@@ -183,6 +195,7 @@ const myMiddleware = store => next => action => {
         const body = {
         }
         console.log("prepare admin");
+
         axios.get(API_HOST + '/seller/get/order', body, {headers: headers})
         .then(response => {
             action.setSellerOrderList(response.data.orderList,store.dispatch);
@@ -192,6 +205,7 @@ const myMiddleware = store => next => action => {
             console.log(err)
             alert("Entering Web Failed!")
         });
+
         axios.get(API_HOST + '/seller/get/user', body, {headers: headers})
         .then(response => {
             action.setSellerUserList(response.data.userList,store.dispatch);
@@ -201,6 +215,7 @@ const myMiddleware = store => next => action => {
             console.log(err)
             alert("Entering Web Failed!")
         });
+
         axios.get(API_HOST + '/seller/get/product', body, {headers: headers})
         .then(response => {
             action.setSellerProductList(response.data.productList,store.dispatch);
@@ -279,6 +294,26 @@ const myMiddleware = store => next => action => {
         .catch(err => {
             console.log(err)
             alert("Add Product Failed!")
+        });
+    }else if(action.type === "ADD_DISCOUNT"){
+        const headers = getHeaders(action.token);
+        const body = { 
+            value: action.value,
+            code: action.code,
+            name: action.name,
+            startDate: action.startDate,
+            endDate: action.endDate
+        }
+        console.log(body);
+        console.log("adding Discount");
+        axios.post(API_HOST + '/seller/add/discount', body, {headers: headers})
+        .then(response => {
+            console.log("Discount have been add");
+            alert("Add Discount Success!")
+        })
+        .catch(err => {
+            console.log(err)
+            alert("Add Discount Failed!")
         });
     }else {
         return next(action)
